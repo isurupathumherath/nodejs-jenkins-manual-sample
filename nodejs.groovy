a62894c2912a14af5057d33347cf40dbb02f86e5
Isuru Pathum Herath <77333662+isurupathumherath@users.noreply.github.com>
2022-01-13 01:43:20 +0530

    nodejs groovy

---
+++ nodejs.groovy
@@ -0,0 +1,21 @@
+job('NodeJS Groovy Project') {
+    scm {
+        git('git://github.com/isurupathumherath/devops-sample.git') {
+            node ->
+                node / gitConfigName('isurupathumherath')
+                node / gitConfigEmail('isurupathumherath@gmail.com')
+        }
+    }
+
+    trigger {
+        scm('H/5 * * * *')
+    }
+
+    wrappers {
+        nodejs('nodejs')
+    }
+
+    steps{
+        shell("npm install")
+    }
+}
